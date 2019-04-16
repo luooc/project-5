0842a5e09aa6028f884279e0a34a296710709151
brinalesser <49353127+brinalesser@users.noreply.github.com>
2019-04-16 19:19:31 -0400

    Update DLList.java

--- Project 5/src/prj5/DLList.java
+++ Project 5/src/prj5/DLList.java
@@ -1,9 +1,25 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+// -- Avni Trasi (avnitrasi)
+// -- James Livingston (jamesrliving)
+// -- Oliver Luo (luooc)
+// -- Sabrina Lesser (brinalesser)
 package prj5;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
 /**
+ * @author Avni Trasi (avnitrasi)
+ * @version 2019.04.15
+ * @author James Livingston (jamesrliving)
+ * @version 2019.04.15
+ * @author Oliver Luo (luooc)
+ * @version 2019.04.15
+ * @author Sabrina Lesser (brinalesser)
+ * @version 2019.04.15
  * @param <T>
  *            The type of object the class will store
  */
