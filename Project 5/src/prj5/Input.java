46aad6e04119ed91f53f4cb542011c270ab82916
brinalesser <49353127+brinalesser@users.noreply.github.com>
2019-04-16 14:48:48 -0400

    Update Input.java

--- Project 5/src/prj5/Input.java
+++ Project 5/src/prj5/Input.java
@@ -1,15 +1,15 @@
 package prj5;
 
 /**
-* @author Avni Trasi (avnitrasi)
-* @version 2019.04.15
-* @author James Livingston (jamesrliving)
-* @version 2019.04.15
-* @author Oliver Luo (luooc)
-* @version 2019.04.15
-* @author Sabrina Lesser (brinalesser)
-* @version 2019.04.15
-*/
+ * @author Avni Trasi (avnitrasi)
+ * @version 2019.04.15
+ * @author James Livingston (jamesrliving)
+ * @version 2019.04.15
+ * @author Oliver Luo (luooc)
+ * @version 2019.04.15
+ * @author Sabrina Lesser (brinalesser)
+ * @version 2019.04.15
+ */
 
 public class Input {
     public Input() {
@@ -21,4 +21,3 @@ public class Input {
         GUIWindow window = new GUIWindow();
     }
 }
-
