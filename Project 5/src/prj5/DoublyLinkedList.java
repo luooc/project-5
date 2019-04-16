fb28de156fe6030f95ddf20d353bb8694232abeb
avnitrasi <49353130+avnitrasi@users.noreply.github.com>
2019-04-15 23:36:07 -0400

    Update DoublyLinkedList.java

--- Project 5/src/prj5/DoublyLinkedList.java
+++ Project 5/src/prj5/DoublyLinkedList.java
@@ -1,5 +1,16 @@
 package prj5;
 
+/**
+* @author Avni Trasi (avnitrasi)
+* @version 2019.04.15
+* @author James Livingston (jamesrliving)
+* @version 2019.04.15
+* @author Oliver Luo (luooc)
+* @version 2019.04.15
+* @author Sabrina Lesser (brinalesser)
+* @version 2019.04.15
+*/
+
 public interface DoublyLinkedList<T>
 {    
     /**
