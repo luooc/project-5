54af6eb4516ac51ef261eed3e499f0a13813179e
avnitrasi <49353130+avnitrasi@users.noreply.github.com>
2019-04-15 23:38:01 -0400

    Update SongReader.java

--- Project 5/src/prj5/SongReader.java
+++ Project 5/src/prj5/SongReader.java
@@ -4,6 +4,17 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/**
+* @author Avni Trasi (avnitrasi)
+* @version 2019.04.15
+* @author James Livingston (jamesrliving)
+* @version 2019.04.15
+* @author Oliver Luo (luooc)
+* @version 2019.04.15
+* @author Sabrina Lesser (brinalesser)
+* @version 2019.04.15
+*/
+
 public class SongReader
 {
     private DLList<Song> songList;
