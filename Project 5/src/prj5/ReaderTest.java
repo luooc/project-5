af6edffafa599ef41b9bcaa83e34f78baffc6b7d
brinalesser <49353127+brinalesser@users.noreply.github.com>
2019-04-16 17:16:11 -0400

    Add files via upload

---
+++ Project 5/src/prj5/ReaderTest.java
@@ -0,0 +1,30 @@
+package prj5;
+
+/**
+ * @author Avni Trasi (avnitrasi)
+ * @version 2019.04.15
+ * @author James Livingston (jamesrliving)
+ * @version 2019.04.15
+ * @author Oliver Luo (luooc)
+ * @version 2019.04.15
+ * @author Sabrina Lesser (brinalesser)
+ * @version 2019.04.15
+ */
+public class ReaderTest {
+    // Fields -----------------------------------------------
+
+    /**
+     * Sets up variables for testing
+     */
+    public void setUp() {
+
+    }
+
+
+    /**
+     * 
+     */
+    public void test() {
+
+    }
+}
---
+++ Project 5/src/prj5/SorterTest.java
@@ -0,0 +1,31 @@
+package prj5;
+
+/**
+ * @author Avni Trasi (avnitrasi)
+ * @version 2019.04.15
+ * @author James Livingston (jamesrliving)
+ * @version 2019.04.15
+ * @author Oliver Luo (luooc)
+ * @version 2019.04.15
+ * @author Sabrina Lesser (brinalesser)
+ * @version 2019.04.15
+ */
+public class SorterTest {
+    // Fields -----------------------------------------------
+
+    /**
+     * Sets up variables for testing
+     */
+    public void setUp() {
+
+    }
+
+
+    /**
+     * 
+     */
+    public void test() {
+
+    }
+
+}
