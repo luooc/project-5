1090d175654d3db37061a262da370790f66373ca
brinalesser <49353127+brinalesser@users.noreply.github.com>
2019-04-16 14:50:08 -0400

    Update Student.java

--- Project 5/src/prj5/Student.java
+++ Project 5/src/prj5/Student.java
@@ -1,81 +1,94 @@
 package prj5;
 
 /**
-* @author Avni Trasi (avnitrasi)
-* @version 2019.04.15
-* @author James Livingston (jamesrliving)
-* @version 2019.04.15
-* @author Oliver Luo (luooc)
-* @version 2019.04.15
-* @author Sabrina Lesser (brinalesser)
-* @version 2019.04.15
-*/
+ * @author Avni Trasi (avnitrasi)
+ * @version 2019.04.15
+ * @author James Livingston (jamesrliving)
+ * @version 2019.04.15
+ * @author Oliver Luo (luooc)
+ * @version 2019.04.15
+ * @author Sabrina Lesser (brinalesser)
+ * @version 2019.04.15
+ */
 
-public class Student
-{
+public class Student {
     private String hobby;
     private String major;
     private String state;
     private String[] songsHeard;
     private String[] songsLiked;
-    
+
+
     /**
      * Constructor for student object
-     * @param hobby param for hobby field
-     * @param major param for major field
-     * @param state param for state field
+     * 
+     * @param hobby
+     *            param for hobby field
+     * @param major
+     *            param for major field
+     * @param state
+     *            param for state field
      */
-    public Student(String hobby, String major, String state, String[] songsHeard, String[] songsLiked)
-    {
+    public Student(
+        String hobby,
+        String major,
+        String state,
+        String[] songsHeard,
+        String[] songsLiked) {
         this.hobby = hobby;
         this.major = major;
         this.state = state;
         this.songsHeard = songsHeard;
         this.songsLiked = songsLiked;
     }
-    
+
+
     /**
      * getter for hobby field
+     * 
      * @return hobby field
      */
-    public String getHobby()
-    {
+    public String getHobby() {
         return hobby;
     }
-    
+
+
     /**
      * getter for major field
+     * 
      * @return major field
      */
-    public String getMajor()
-    {
+    public String getMajor() {
         return major;
     }
-    
+
+
     /**
      * getter for state field
+     * 
      * @return state field
      */
-    public String getState()
-    {
+    public String getState() {
         return state;
     }
-    
+
+
     /**
      * getter for songsHeard field
+     * 
      * @return songsHeard field
      */
-    public String[] getSongsHeard()
-    {
+    public String[] getSongsHeard() {
         return songsHeard;
     }
-    
+
+
     /**
      * getter for songsLiked field
+     * 
      * @return songsLiked field
      */
-    public String[] getSongsLiked()
-    {
+    public String[] getSongsLiked() {
         return songsLiked;
     }
 }
