e265fa5f40fcacfd5f6cc4e00991283981bde21e
brinalesser <49353127+brinalesser@users.noreply.github.com>
2019-04-15 22:06:10 -0400

    Update DLList.java

--- Project 5/src/prj5/DLList.java
+++ Project 5/src/prj5/DLList.java
@@ -1,180 +1,516 @@
-package prj5;
+package doublylinkedlist;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class DLList<T> implements DoublyLinkedList<T>, Iterable<T>
-{
-    private Node<T> head;
-    private Node<T> tail;
-    private int size;
-    
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
+/**
+ * This provides implementation for some of the LList methods.
+ *
+ * @author Mark Wiggans (mmw125)
+ * @version 3/29/15
+ * @author Eric Williamson
+ * @version 10/30/15
+ * @author maellis1
+ * @version 11/1/15
+ * @author Sabrina Lesser (brinalesser)
+ * @version 4/15/2019
+ * @param <E>
+ *            The type of object the class will store
+ */
+public class DLList<E> {
+    /**
+     * This represents a node in a doubly linked list. This node stores data, a
+     * pointer to the node before it in the list, and a pointer to the node
+     * after it in the list
+     *
+     * @param <E>
+     *            This is the type of object that this class will store
+     * @author Mark Wiggans (mmw125)
+     * @version 4/14/2015
+     */
+    private static class Node<E> {
+        private Node<E> next;
+        private Node<E> previous;
+        private E data;
 
-    @Override
-    public int size() {
-        return size;
-    }
 
-    @Override
-    public void clear() {
-        size = 0;
-        head = null;
-        tail = null;
-    }
-
-    @Override
-    public boolean contains(T obj) {
-        Node current = head;
-        while(current != null){
-            if(current.data.equals(obj)){
-                return true;
-            }
-            current = current.next;
-        }
-        return false;
-    }
-
-    @Override
-    public T get(int index) {
-        Node current = head;
-        for(int i = 0; i <= index; i++){
-            current = current.next;
-        }
-        return current.data;
-    }
-
-    @Override
-    public void add(T newEntry) {
-        
-        
-    }
-
-    @Override
-    public void add(int index, T obj) {
-        
-        
-    }
-
-    @Override
-    public boolean remove(int index) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean remove(T obj) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public Iterator<T> iterator()
-    {
-        return new DLListIterator<T>();
-    }
-
-    private static class Node<T>
-    {
-        private Node<T> next;
-        private Node<T> previous;
-        private T data;
-    
         /**
          * Creates a new node with the given data
          *
          * @param d
          *            the data to put inside the node
          */
-        public Node(T d) {
+        public Node(E d) {
             data = d;
         }
-    
+
+
         /**
          * Sets the node after this node
          *
          * @param n
          *            the node after this one
          */
-        public void setNext(Node<T> n) {
+        public void setNext(Node<E> n) {
             next = n;
         }
-    
+
+
         /**
          * Sets the node before this one
          *
          * @param n
          *            the node before this one
          */
-        public void setPrevious(Node<T> n) {
+        public void setPrevious(Node<E> n) {
             previous = n;
         }
-    
+
+
         /**
          * Gets the next node
          *
          * @return the next node
          */
-        public Node<T> next() {
+        public Node<E> next() {
             return next;
         }
-    
+
+
         /**
          * Gets the node before this one
          *
          * @return the node before this one
          */
-        public Node<T> previous() {
+        public Node<E> previous() {
             return previous;
         }
-    
+
+
         /**
          * Gets the data in the node
          *
          * @return the data in the node
          */
-        public T getData() {
+        public E getData() {
             return data;
         }
     }
 
-    private class DLListIterator<A> implements Iterator<T>
-    {
-        private Node<T> nextNode;
-        private T nextCalled;
-        
-        public DLListIterator()
-        {
-            nextNode = head.next();
-            nextCalled = null;
+    /**
+     * How many nodes are in the list
+     */
+    private int size;
+
+    /**
+     * The first node in the list. THIS IS A SENTINEL NODE AND AS SUCH DOES NOT
+     * HOLD ANY DATA. REFER TO init()
+     */
+    private Node<E> head;
+
+    /**
+     * The last node in the list. THIS IS A SENTINEL NODE AND AS SUCH DOES NOT
+     * HOLD ANY DATA. REFER TO init()
+     */
+    private Node<E> tail;
+
+
+    /**
+     * Create a new DLList object.
+     */
+    public DLList() {
+        init();
+    }
+
+
+    /**
+     * Initializes the object to have the head and tail nodes
+     */
+    private void init() {
+        head = new DLList.Node<E>(null);
+        tail = new DLList.Node<E>(null);
+        head.setNext(tail);
+        tail.setPrevious(head);
+        size = 0;
+    }
+
+
+    /**
+     * Checks if the array is empty
+     *
+     * @return true if the array is empty
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    /**
+     * Gets the number of elements in the list
+     *
+     * @return the number of elements
+     */
+    public int size() {
+        return size;
+    }
+
+
+    /**
+     * Removes all of the elements from the list
+     */
+    public void clear() {
+        init();
+    }
+
+
+    /**
+     * Checks if the list contains the given object
+     *
+     * @param obj
+     *            the object to check for
+     * @return true if it contains the object
+     */
+    public boolean contains(E obj) {
+        return lastIndexOf(obj) != -1;
+    }
+
+
+    /**
+     * Gets the object at the given position
+     *
+     * @param index
+     *            where the object is located
+     * @return The object at the given position
+     * @throws IndexOutOfBoundsException
+     *             if there no node at the given index
+     */
+    public E get(int index) {
+        return getNodeAtIndex(index).getData();
+    }
+
+
+    /**
+     * Adds a element to the end of the list
+     *
+     * @param newEntry
+     *            the element to add to the end
+     */
+    public void add(E newEntry) {
+        add(size(), newEntry);
+    }
+
+
+    /**
+     * Adds the object to the position in the list
+     *
+     * @param index
+     *            where to add the object
+     * @param obj
+     *            the object to add
+     * @throws IndexOutOfBoundsException
+     *             if index is less than zero or greater than size
+     * @throws IllegalArgumentException
+     *             if obj is null
+     */
+    public void add(int index, E obj) {
+        if (index < 0 || size < index) {
+            throw new IndexOutOfBoundsException();
         }
-        
+        if (obj == null) {
+            throw new IllegalArgumentException("Cannot add null "
+                + "objects to a list");
+        }
+
+        Node<E> nodeAfter;
+        if (index == size) {
+            nodeAfter = tail;
+        }
+        else {
+            nodeAfter = getNodeAtIndex(index);
+        }
+
+        Node<E> addition = new Node<E>(obj);
+        addition.setPrevious(nodeAfter.previous());
+        addition.setNext(nodeAfter);
+        nodeAfter.previous().setNext(addition);
+        nodeAfter.setPrevious(addition);
+        size++;
+
+    }
+
+
+    /**
+     * gets the node at that index
+     * 
+     * @param index
+     * @return node at index
+     */
+    private Node<E> getNodeAtIndex(int index) {
+        if (index < 0 || size() <= index) {
+            throw new IndexOutOfBoundsException("No element exists at "
+                + index);
+        }
+        Node<E> current = head.next(); // as we have a sentinel node
+        for (int i = 0; i < index; i++) {
+            current = current.next();
+        }
+        return current;
+    }
+
+
+    /**
+     * Gets the last time the given object is in the list
+     *
+     * @param obj
+     *            the object to look for
+     * @return the last position of it. -1 If it is not in the list
+     */
+    public int lastIndexOf(E obj) {
+        /*
+         * We should go from the end of the list as then we an stop once we find
+         * the first one
+         */
+        Node<E> current = tail.previous();
+        for (int i = size() - 1; i >= 0; i--) {
+            if (current.getData().equals(obj)) {
+                return i;
+            }
+            current = current.previous();
+        }
+        return -1; // if we do not find it
+    }
+
+
+    /**
+     * Removes the element at the specified index from the list
+     *
+     * @param index
+     *            where the object is located
+     * @throws IndexOutOfBoundsException
+     *             if there is not an element at the index
+     * @return true if successful
+     */
+    public boolean remove(int index) {
+        Node<E> nodeToBeRemoved = getNodeAtIndex(index);
+        nodeToBeRemoved.previous().setNext(nodeToBeRemoved.next());
+        nodeToBeRemoved.next().setPrevious(nodeToBeRemoved.previous());
+        size--;
+        return true;
+    }
+
+
+    /**
+     * Removes the first object in the list that .equals(obj)
+     *
+     * @param obj
+     *            the object to remove
+     * @return true if the object was found and removed
+     */
+
+    public boolean remove(E obj) {
+        Node<E> current = head.next();
+        while (!current.equals(tail)) {
+            if (current.getData().equals(obj)) {
+                current.previous().setNext(current.next());
+                current.next().setPrevious(current.previous());
+                size--;
+                return true;
+            }
+            current = current.next();
+        }
+        return false;
+    }
+
+
+    /**
+     * Returns a string representation of the list If a list contains A, B, and
+     * C, the following should be returned "{A, B, C}" (Without the quotations)
+     *
+     * @return a string representing the list
+     */
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("{");
+        if (!isEmpty()) {
+            Node<E> currNode = head.next();
+            while (currNode != tail) {
+                E element = currNode.getData();
+                builder.append(element.toString());
+                if (currNode.next != tail) {
+                    builder.append(", ");
+                }
+                currNode = currNode.next();
+            }
+        }
+
+        builder.append("}");
+        return builder.toString();
+    }
+
+
+    /**
+     * Creates an iterator for a DLList
+     */
+    private class DLListIterator<A> implements Iterator<E> {
+        // Fields
+        private Node<E> nextNode;
+        private boolean nextCalled;
+
+
+        // Constructor
+        /**
+         * Creates a new DLListIterator
+         */
+        public DLListIterator() {
+            nextNode = head;
+            nextCalled = false;
+        }
+
+
+        /**
+         * Checks if there are more elements in the list
+         *
+         * @return true if there are more elements in the list
+         */
         @Override
-        public boolean hasNext()
-        {
-            return nextNode == tail;
+        public boolean hasNext() {
+            return nextNode.next().getData() != null;
         }
+
 
         /**
          * Gets the next value in the list
+         *
          * @return the next value
          * @throws NoSuchElementException
          *             if there are no nodes left in the list
          */
         @Override
-        public T next() {
-            if (!hasNext())
-            {
-                throw new NoSuchElementException("Illegal call to next(); " + 
-                                             "iterator is after end of list.");
+        public E next() {
+            if (hasNext()) {
+                nextNode = nextNode.next();
+                nextCalled = true;
+                return nextNode.getData();
             }
-            Node<T> returnNode = nextNode;
-            nextNode = nextNode.next();
-                
-            return null;
+            else {
+                throw new NoSuchElementException("Illegal call to next(); "
+                    + "iterator is after end of list.");
+            }
         }
+
+
+        /**
+         * Removes the last object returned with next() from the list
+         *
+         * @throws IllegalStateException
+         *             if next has not been called yet
+         *             and if the element has already been removed
+         */
+        @Override
+        public void remove() {
+            if (nextCalled) {
+                nextNode.previous().setNext(nextNode.next());
+                nextNode.next().setPrevious(nextNode.previous());
+                size--;
+                nextCalled = false;
+            }
+            else {
+                throw new IllegalStateException("Illegal call to remove(); "
+                    + "next() was not called.");
+            }
+
+        }
+    }
+
+
+    /**
+     * Iterator method creates Iterator object
+     *
+     * @return new Iterator object
+     */
+    public Iterator<E> iterator() {
+        return new DLListIterator<E>();
+    }
+
+
+    private class RDLListIterator<A> implements Iterator<E> {
+        // Fields
+        private Node<E> nextNode;
+        private boolean nextCalled;
+
+
+        // Constructor
+        /**
+         * Creates a new DLListIterator
+         */
+        public RDLListIterator() {
+            nextNode = tail;
+            nextCalled = false;
+        }
+
+
+        /**
+         * Checks if there are more elements in the list
+         *
+         * @return true if there are more elements in the list
+         */
+        @Override
+        public boolean hasNext() {
+            return nextNode.previous().getData() != null;
+        }
+
+
+        /**
+         * Gets the next value in the list
+         *
+         * @return the next value
+         * @throws NoSuchElementException
+         *             if there are no nodes left in the list
+         */
+        @Override
+        public E next() {
+            if (hasNext()) {
+                nextNode = nextNode.previous();
+                nextCalled = true;
+                return nextNode.getData();
+            }
+            else {
+                throw new NoSuchElementException("Illegal call to next(); "
+                    + "iterator is after end of list.");
+            }
+        }
+
+
+        /**
+         * Removes the last object returned with next() from the list
+         *
+         * @throws IllegalStateException
+         *             if next has not been called yet
+         *             and if the element has already been removed
+         */
+        @Override
+        public void remove() {
+            if (nextCalled) {
+                nextNode.previous().setNext(nextNode.next());
+                nextNode.next().setPrevious(nextNode.previous());
+                size--;
+                nextCalled = false;
+            }
+            else {
+                throw new IllegalStateException("Illegal call to remove(); "
+                    + "next() was not called.");
+            }
+
+        }
+    }
+
+
+    /**
+     * Iterator method creates Iterator object
+     *
+     * @return new Iterator object
+     */
+    public Iterator<E> reverseIterator() {
+        return new RDLListIterator<E>();
     }
 }
