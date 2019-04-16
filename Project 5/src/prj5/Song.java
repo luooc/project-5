107c40552a0af8c93d7b1859b97b724215da32d1
brinalesser <49353127+brinalesser@users.noreply.github.com>
2019-04-16 14:49:36 -0400

    Update Song.java

--- Project 5/src/prj5/Song.java
+++ Project 5/src/prj5/Song.java
@@ -1,71 +1,79 @@
 package prj5;
 
 /**
-* @author Avni Trasi (avnitrasi)
-* @version 2019.04.15
-* @author James Livingston (jamesrliving)
-* @version 2019.04.15
-* @author Oliver Luo (luooc)
-* @version 2019.04.15
-* @author Sabrina Lesser (brinalesser)
-* @version 2019.04.15
-*/
+ * @author Avni Trasi (avnitrasi)
+ * @version 2019.04.15
+ * @author James Livingston (jamesrliving)
+ * @version 2019.04.15
+ * @author Oliver Luo (luooc)
+ * @version 2019.04.15
+ * @author Sabrina Lesser (brinalesser)
+ * @version 2019.04.15
+ */
 
-public class Song
-{
+public class Song {
     private String artist;
     private String title;
     private String genre;
     private int date;
-    
+
+
     /**
      * Constructor for Song object
-     * @param artist param for artist field
-     * @param title param for title field
-     * @param genre param for genre field
-     * @param date param for date field
+     * 
+     * @param artist
+     *            param for artist field
+     * @param title
+     *            param for title field
+     * @param genre
+     *            param for genre field
+     * @param date
+     *            param for date field
      */
-    public Song(String artist, String title, int date, String genre)
-    {
+    public Song(String artist, String title, int date, String genre) {
         this.artist = artist;
         this.title = title;
         this.genre = genre;
         this.date = date;
     }
-    
+
+
     /**
      * getter for artist field
+     * 
      * @return artist field
      */
-    public String getArtist()
-    {
+    public String getArtist() {
         return artist;
     }
-    
+
+
     /**
      * getter for title field
+     * 
      * @return
      */
-    public String getTitle()
-    {
+    public String getTitle() {
         return title;
     }
-    
+
+
     /**
      * getter for genre field
+     * 
      * @return genre field
      */
-    public String getGenre()
-    {
+    public String getGenre() {
         return genre;
     }
-    
+
+
     /**
      * getter for date field
+     * 
      * @return date field
      */
-    public int getDate()
-    {
+    public int getDate() {
         return date;
     }
 }
