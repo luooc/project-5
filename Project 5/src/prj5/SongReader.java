dbbebbe6a655db9e7afa4f0faa999ec0d719e995
Oliver Luo <49353131+luooc@users.noreply.github.com>
2019-04-15 22:13:21 -0400

    Update SongReader.java

--- Project 5/src/prj5/SongReader.java
+++ Project 5/src/prj5/SongReader.java
@@ -1,5 +1,9 @@
 package prj5;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class SongReader
 {
     private DLList<Song> songList;
@@ -10,13 +14,31 @@ public class SongReader
         
     }
     
-    private DLList<Song> readSongFile(String fileName)
+    private DLList<Song> readSongFile(String fileName) throws FileNotFoundException
     {
+        Scanner file = new Scanner(new File(fileName));
+        DLList<Song> copy = new DLList<Song>();
+        
+        while (file.hasNext())
+        {
+            String nextLine = file.nextLine();
+            if (!nextLine.equals("Song Title,Artist,Year,Genre"))
+            {
+                String[] info = nextLine.split(",");
+                if (info.length == 4)
+                {
+                    Song newSong = new Song(info[0], info[1], Integer.parseInt(info[2]), info[3]);
+                    copy.add(newSong);
+                }
+            }
+        }
+        file.close();
+        return copy;
         
     }
     
-    private DLList<Student> readStudentFile(String fileName)
+    private DLList<Student> readStudentFile(String fileName) throws FileNotFoundException
     {
-        
+        Scanner file = new Scanner(new File(fileName));
     }
 }
