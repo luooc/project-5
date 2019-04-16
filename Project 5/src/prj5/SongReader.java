9d68c23d86ce9ba75ff327792d486083de1a87a0
Oliver Luo <49353131+luooc@users.noreply.github.com>
2019-04-16 01:07:01 -0400

    Update SongReader.java

--- Project 5/src/prj5/SongReader.java
+++ Project 5/src/prj5/SongReader.java
@@ -5,6 +5,17 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+* @author Avni Trasi (avnitrasi)
+* @version 2019.04.15
+* @author James Livingston (jamesrliving)
+* @version 2019.04.15
+* @author Oliver Luo (luooc)
+* @version 2019.04.15
+* @author Sabrina Lesser (brinalesser)
+* @version 2019.04.15
+*/
+
 public class SongReader
 {
     private DLList<Song> songList;
