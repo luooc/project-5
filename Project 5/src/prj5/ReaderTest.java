37652f8fa85e2cca02e46aebd949f065bbf974ba
brinalesser <49353127+brinalesser@users.noreply.github.com>
2019-04-16 18:06:13 -0400

    Update ReaderTest.java

--- Project 5/src/prj5/ReaderTest.java
+++ Project 5/src/prj5/ReaderTest.java
@@ -1,5 +1,8 @@
 package prj5;
 
+import static org.junit.Assert.assertEquals;
+import java.io.FileNotFoundException;
+
 /**
  * @author Avni Trasi (avnitrasi)
  * @version 2019.04.15
@@ -12,19 +15,36 @@ package prj5;
  */
 public class ReaderTest {
     // Fields -----------------------------------------------
+    private Reader reader;
+
 
     /**
      * Sets up variables for testing
      */
     public void setUp() {
-
+        try {
+            reader = new Reader("SongList2018.csv", "MusicSurveyData2018.csv");
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 
     /**
-     * 
+     * Tests to make sure the method getSongs works as intended
      */
-    public void test() {
+    public void testGetSongs() {
+        DLList<Song> songs = new DLList<Song>();
+        assertEquals(reader.getSongs(), songs);
+    }
 
+
+    /**
+     * Tests to make sure the method getStudents works as intended
+     */
+    public void testGetStudents() {
+        DLList<Student> students = new DLList<Student>();
+        assertEquals(reader.getStudents(), students);
     }
 }
