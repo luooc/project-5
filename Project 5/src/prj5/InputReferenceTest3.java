8f7ca18f6cdf87fc4d6f85c6c18848ac951e69d1
jamesrliving <49353193+jamesrliving@users.noreply.github.com>
2019-04-18 00:01:10 -0400

    Update InputReferenceTest3.java

--- Project 5/src/prj5/InputReferenceTest3.java
+++ Project 5/src/prj5/InputReferenceTest3.java
@@ -9,7 +9,7 @@ import student.testingsupport.annotations.Hint;
  * @author margaretellis
  * @version 11-12-2015
  */
-public class InputReferenceTest3 extends student.TestCase {
+public class InputTest extends student.TestCase {
     private String fileName;
     private Scanner fileData;
 
